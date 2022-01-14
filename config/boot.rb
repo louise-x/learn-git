3b03d876989559ac4ed0824fd7a365ee1a9331ac
louis-x <louis-x@test.com>
2022-01-14 21:27:54 +0800

    config bootsnap cache dir

--- config/boot.rb
+++ config/boot.rb
@@ -3,3 +3,11 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 # test bootsnap
 require 'bootsnap/setup'
+
+# if %w[s server c consol].any? {|a| ARGV.include?(a)}
+#   puts "=> Booting Rails"
+# end
+shared_dir = "#{app_dir}/shared"
+Bootsnap.setup(
+    cache_dir: '#{shared_dir}/tmp/cache'
+)
